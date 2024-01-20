9e237ba5c98abc3a3537ac2776643853c8e3a792
KarthickRP1 <157097092+KarthickRP1@users.noreply.github.com>
2024-01-20 12:58:08 +0530

    Add files via upload

---
+++ weather app.py
@@ -0,0 +1,19 @@
+import requests
+
+api_key = '7191b330a5240783700ebfb8b696fb9a'
+
+user_input = input("Enter city: ")
+
+weather_data = requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
+
+#print(weather_data.json())  --use this code for when we need all data of weather.
+#i'm just printing climate and temperature.
+if weather_data.json()['cod'] == '404':
+    print("No City Found")
+else:
+    weather = weather_data.json()['weather'][0]['main']
+    temp = round(weather_data.json()['main']['temp'])
+
+    print(f"The weather in {user_input} is: {weather}")
+    print(f"The temperature in {user_input} is: {temp}ºF")
